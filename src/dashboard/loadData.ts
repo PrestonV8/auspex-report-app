c7e7f3654949d6df6e495367b62db6141227978a
PrestonV <PrestonViado@outlook.com>
2026-07-28 15:14:29 -0400

    feat: made separate parsing functions for parsing the JSON or JSONL

--- src/dashboard/loadData.ts
+++ src/dashboard/loadData.ts
@@ -9,39 +9,49 @@ import { TrendEntry, RunMetadata } from "../shared/types.js";
  * 3. parse each line as JSON
  * 4. Collect every parsed entry, from every file, into one big combined array
  */
+export function loadTrendEntries(): TrendEntry[] {
+    const files = fs.globSync("data/*.jsonl");
+    const result: TrendEntry[] = [];
 
-const files = fs.globSync("data/*.jsonl");
-const result: TrendEntry[] = [];
+    for (const filePath of files) {
+        const raw = fs.readFileSync(filePath, "utf-8");
+        const lines = raw.split("\n");
 
-for (const filePath of files) {
-    const raw = fs.readFileSync(filePath, "utf-8");
-    const lines = raw.split("\n");
-
-    for (const line of lines) {
-        // if the line is not an empty string, do this
-        if (line !== "") {
-            const entry = JSON.parse(line);
-            result.push(entry);
+        for (const line of lines) {
+            // if the line is not an empty string, do this
+            if (line !== "") {
+                const entry = JSON.parse(line);
+                result.push(entry);
+            }
         }
     }
 
+    // to print the output of the parsing of result
+    //console.log(result);
+
+    return result;
 }
 
-// to print the output of the parsing of result
-//console.log(result);
 
-const runFiles = fs.globSync("data/runs/*.json");
-const runs: RunMetadata[] = [];
 
-for (const filePath of runFiles) {
-    const raw = fs.readFileSync(filePath, "utf-8");
 
-    // if the line is not an empty string, do this
-    if (raw !== "") {
-        const currentRun = JSON.parse(raw);
-        runs.push(currentRun);
+export function loadRuns(): RunMetadata[] {
+    const runFiles = fs.globSync("data/runs/*.json");
+    const runs: RunMetadata[] = [];
+
+    for (const filePath of runFiles) {
+        const raw = fs.readFileSync(filePath, "utf-8");
+
+        // if the line is not an empty string, do this
+        if (raw !== "") {
+            const currentRun = JSON.parse(raw);
+            runs.push(currentRun);
+        }
     }
+
+    // to print the output of the parsing of runs
+    // console.log(runs);
+    return runs;
 }
 
-// to print the output of the parsing of runs
-// console.log(runs);
+
