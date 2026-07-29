17b931956628ca8eab75ecdcdf44ec8afb4917b7
PrestonV <PrestonViado@outlook.com>
2026-07-29 14:12:38 -0400

    feat: implemented analytics.ts to calculate test pass rate and provide percentage of success rates

---
+++ src/dashboard/analytics.ts
@@ -0,0 +1,35 @@
+import { TrendEntry } from "../shared/types.js";
+import { loadTrendEntries } from "./loadData.js";
+
+// Helper function to group the entries into Key Value pairs
+export function groupEntries(entries: TrendEntry[]): Record<string, TrendEntry[]> {
+    const grouped: Record<string, TrendEntry[]> = {};
+    for (const entry of entries) {
+        if (!grouped[entry.runId]) {
+            grouped[entry.runId] = [];
+        }
+        grouped[entry.runId].push(entry);
+    }
+
+    return grouped;
+}
+
+
+// Helper function to calculate the pass rate of tests
+export function calculatePassRates(grouped: Record<string, TrendEntry[]>): Record<string, number> {
+    const passRates: Record<string, number> = {};
+
+    for (const runId in grouped) {
+        const entries = grouped[runId];
+
+        const totalCount = entries.length;
+        const passedCount = entries.filter((entry) => {
+            return entry.status === "passed";
+        }).length;
+        const rate = (passedCount / totalCount) * 100;
+
+        passRates[runId] = rate;
+    }
+
+    return passRates;
+}
--- src/dashboard/insights-dashboard.ts
+++ src/dashboard/insights-dashboard.ts
@@ -1,9 +1,10 @@
 import minimist from "minimist";
 import { loadTrendEntries, loadRuns } from "./loadData.js";
+import { TrendEntry } from "../shared/types.js";
+import { groupEntries, calculatePassRates } from "./analytics.js";
 
 
 // used to filter the entries by amount of days since current dat. Default is over the past 30 days
 const daysArg = minimist(process.argv.slice(2)).days ?? 30;
 
-console.log(loadTrendEntries(daysArg));
-console.log(loadRuns(daysArg));
+console.log(calculatePassRates(groupEntries(loadTrendEntries(daysArg))));
