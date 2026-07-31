c02afc07bd54a4e8aa90df854ae9d4f6012b201b
PrestonV <PrestonViado@outlook.com>
2026-07-31 15:29:27 -0400

    feat: implemented the flakyness leaderboard pipelien to add onto dashboard creation

--- src/dashboard/insights-dashboard.ts
+++ src/dashboard/insights-dashboard.ts
@@ -1,9 +1,8 @@
 import minimist from "minimist";
 import { loadTrendEntries, loadRuns } from "./loadData.js";
-import { TrendEntry } from "../shared/types.js";
-import { groupEntries, calculatePassRates, calculateAveragePassRate } from "./analytics.js";
+import { groupEntries, calculatePassRates, calculateAveragePassRate, calculateFlakyLeaderboard } from "./analytics.js";
 import fs from "node:fs";
-import { renderPassRateTable, renderPage } from "./render.js";
+import { renderPassRateTable, renderPage, renderFlakyLeaderboard } from "./render.js";
 
 
 // used to filter the entries by amount of days since current dat. Default is over the past 30 days
@@ -12,12 +11,21 @@ const daysArg = minimist(process.argv.slice(2)).days ?? 30;
 //console.log(calculatePassRates(groupEntries(loadTrendEntries(daysArg))));
 //console.log(renderPassRateTable(daysArg))
 
-// generate the sequence in which to create the dashboard
+// Pass Rate table pipeline
 const entries = loadTrendEntries(daysArg);
-const grouped = groupEntries(entries, "runId");
-const passRates = calculatePassRates(grouped);
+const groupedRuns = groupEntries(entries, "runId");
+const passRates = calculatePassRates(groupedRuns);
 const averagePassRate = calculateAveragePassRate(passRates);
 const passRateTable = renderPassRateTable(passRates, averagePassRate);
-const dashboard = renderPage(passRateTable);
+
+
+// Flakyness Leaderboard pipeline
+const groupedTests = groupEntries(entries, "testId");
+const flakyness = calculateFlakyLeaderboard(groupedTests);
+const flakyLeaderboard = renderFlakyLeaderboard(flakyness);
+
+// Generating the full dashboard
+const dashboardContent = passRateTable + flakyLeaderboard;
+const dashboard = renderPage(dashboardContent);
 
 fs.writeFileSync(`./src/dashboard/insights.html`, dashboard);
